3ca56aa50c0422cf9857f67a45003b44f7bf3ec8
Waihon Yew <yewwaihon@gmail.com>
2022-05-21 23:22:49 +0800

    Use withAnimation() to explicitly animate a button

--- Animations2/ContentView.swift
+++ Animations2/ContentView.swift
@@ -8,6 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var animationAmount = 0.0
+
+    var body: some View {
+        Button("Tap Me") {
+            withAnimation {
+                animationAmount += 360
+            }
+        }
+        .padding(50)
+        .background(.red)
+        .foregroundColor(.white)
+        .clipShape(Circle())
+        .rotation3DEffect(.degrees(animationAmount), axis: (x: 0, y: 1, z: 0))
+    }
+}
+
+struct BindingContentView: View {
     @State private var animationAmount = 1.0
 
     var body: some View {

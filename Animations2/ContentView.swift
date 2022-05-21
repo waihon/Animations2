097bc7621d20c58e77783a1d72e39b14f80f8d35
Waihon Yew <yewwaihon@gmail.com>
2022-05-21 15:42:09 +0800

    Increate the button size by 50% every time it's stopped

--- Animations2/ContentView.swift
+++ Animations2/ContentView.swift
@@ -8,14 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var animationAmount = 1.0
+
     var body: some View {
         Button("Tap Me") {
-            // Do nothing
+            animationAmount += 0.5  // 50%
         }
         .padding(50)
         .background(.red)
         .foregroundColor(.white)
         .clipShape(Circle())
+        .scaleEffect(animationAmount)
     }
 }
 

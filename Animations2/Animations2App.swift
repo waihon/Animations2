287d609c304d149a5e24590efb7ae4f4ef161394
Waihon Yew <yewwaihon@gmail.com>
2022-05-21 14:40:43 +0800

    Initial Commit

---
+++ Animations2/Animations2App.swift
@@ -0,0 +1,17 @@
+//
+//  Animations2App.swift
+//  Animations2
+//
+//  Created by Waihon Yew on 21/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct Animations2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

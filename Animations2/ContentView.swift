50430c03828263d9e9cd9e89eca59d02e70f4f68
Waihon Yew <yewwaihon@gmail.com>
2022-05-22 14:58:12 +0800

    Replace blue rectangle with red rectangle on tap

--- Animations2/ContentView.swift
+++ Animations2/ContentView.swift
@@ -8,8 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isShowingRed = false
+
     var body: some View {
-        Text("Hello, world!")
+        ZStack {
+            Rectangle()
+                .fill(.blue)
+                .frame(width: 200, height: 200)
+
+            if isShowingRed {
+                Rectangle()
+                    .fill(.red)
+                    .frame(width: 200, height: 200)
+            }
+        }
+        .onTapGesture {
+            withAnimation {
+                isShowingRed.toggle()
+            }
+        }
     }
 }
 
